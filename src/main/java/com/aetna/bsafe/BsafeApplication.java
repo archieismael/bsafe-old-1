07e60af4ba20520f133cf4d4f9ec8cb87346cb18
archieismael <archie_ismael@yahoo.com>
2021-11-25 11:43:45 +0000

    test

---
+++ src/main/java/com/aetna/bsafe/BsafeApplication.java
@@ -0,0 +1,13 @@
+package com.aetna.bsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aetna/bsafe/BsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aetna.bsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
