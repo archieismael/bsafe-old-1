bfdbfcfbfc607532865ff7ef4569266297841b94
archieismael <archie_ismael@yahoo.com>
2021-11-25 11:55:04 +0000

    test

---
+++ src/main/java/com/simplilearn/calculator/CalculatorApplication.java
@@ -0,0 +1,17 @@
+package com.simplilearn.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+	
+	int sum(int a, int b){
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
